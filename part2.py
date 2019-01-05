5cdc2172edd7f3bf47ee151ba23f723867613684
Gregory Salinas <asalinas720@gmail.com>
2019-01-04 18:25:32 -0800

    Need to convert fields to CSV
    part2 now accepts an array of strings from user input and can match the strings in the text. Final Step should be to convert matches to CSV

--- part2.py
+++ part2.py
@@ -2,13 +2,22 @@ import pandas as pd
 import settings
 import datetime
 
+## Work on Parsing Text ##
+
+
+def find_fields(field):
+    try:
+        with open(file_name + '.txt') as file_output:
+            return print(file_output.read().count(field))
+    except FileNotFoundError:
+        print('No file')
+# Convert user inputs to CSV
+
+
 today = datetime.date.today()
 file_name = 'test-2019-01-04'  # settings.file_name + + '-%s' % today
+fields_strings = input('Enter fields to search seperated by a comma: ')
+fields_list = fields_strings.split(',')
 
-file_output = open(file_name + '.txt', 'r')
-while True:
-    text = file_output.readline()
-    if 'Hi ' in text:
-        print(text)
-
-## Work on Parsing Text ##
+for field in fields_list:
+    find_fields(field)
