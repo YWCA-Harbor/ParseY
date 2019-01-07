99bff0f46628ba2f044b07bfb00e751ee59fce56
Gregory Salinas <asalinas720@gmail.com>
2019-01-06 19:32:53 -0800

    Correct length of items in list
    90% ready to go

--- part2.py
+++ part2.py
@@ -1,3 +1,4 @@
+from itertools import repeat
 import settings
 import datetime
 import csv
@@ -8,9 +9,11 @@ def find_fields(field):
         with open(text_file_name + '.txt') as file_output:
             for line in file_output:
                 if line.count(field):
-                    turn_into_CSV(field, line)
+                    csv_obj = turn_into_CSV(field, line)
     except FileNotFoundError:
         print('Sorry, there was an error due to a text file not being found')
+
+    return csv_obj
 # Read only lines that contain the word in the parameter `field`
 
 
@@ -34,25 +37,25 @@ def turn_into_CSV(column, value):
             if len(row) != cc_length:
                 row.append(value)
                 break
-            # Problem is here #
-            # elif index == rc_length - 1:
-            #     new_row.append(row)
-            #     rc_arr.append(new_row)
+            elif index == rc_length - 1:
+                new_row.extend(repeat('', cc_length - 1))
+                new_row.append(value)
+                rc_arr.append(new_row)
 
-    return print(cc_arr, rc_arr, csv_file_name)
-    # write_CSV(cc_arr, rc_arr, csv_file_name)
+    return [cc_arr, rc_arr]
 # Convert field and line to CSV
 
 
 def write_CSV(column, rows, file_name):
     csv_file = file_name + '-%s' % today
+    print(column, rows, file_name)
 
-    with open(csv_file + '.csv', 'w') as new_csv:
-        csv_writer = csv.writer(new_csv)
-        csv_writer.writerow(column)
+    # with open(csv_file + '.csv', 'w') as new_csv:
+    #     csv_writer = csv.writer(new_csv)
+    #     csv_writer.writerow(column)
 
-        for row in rows:
-            csv_writer.writerow(row)
+    #     for row in rows:
+    #         csv_writer.writerow(row)
 # Writes CSV File
 
 
@@ -75,5 +78,10 @@ fields_strings = input('Enter fields to search seperated by a comma: ')
 fields_list = fields_strings.replace(' ', '').split(',')
 csv_file_name = input('Finally, create a name for your Excel Spreadsheet: ')
 
-for field in fields_list:
-    find_fields(field)
+for index, field in enumerate(fields_list):
+    csv_obj = find_fields(field)
+
+column = csv_obj[0]
+rows = csv_obj[1]
+
+write_CSV(column, rows, csv_file_name)
